0bb30e889c9a0d670b3dc11b3018e16dcc353e0c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-23 02:37:42 -0400

    Add functionality to display numbers on screen after number buttons clicked

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+// Select elements
+const numberBtns = document.querySelectorAll('.number');
+const screen = document.querySelector('.screen');
+
+// Stores current value displayed on the calculator (except intial value)
+let displayValue = null;
+
+// Adds x and y
 function add(x, y) {
     return x + y;
 };
 
+// Subtracts x and y
 function subtract(x, y) {
     return x - y;
 };
 
+// Multiplies x and y
 function multiply(x, y) {
     return x * y;
   };
 
+// Divides x and y
 function divide(x, y) {
     return x / y;
 }
 
+// Calls the function associated with operator with x and y as inputs
 function operate(operator, x, y) {
     if (operator === '+') {
         return add(x,y);
@@ -25,3 +37,17 @@ function operate(operator, x, y) {
         return divide(x,y);
     }
 }
+
+numberBtns.forEach(num => {
+    num.addEventListener('click', () => {
+        if (displayValue === null) {
+            screen.textContent = num.textContent;
+            displayValue = num.textContent;
+            console.log(displayValue);
+        } else {
+            screen.textContent = screen.textContent + num.textContent;
+            displayValue += num.textContent;
+            console.log(displayValue);
+        }
+    })
+})
